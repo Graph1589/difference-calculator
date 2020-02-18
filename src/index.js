d855908232439f325019878cecf6b143731a0daa
Станислав Рожнов <stanislavroznov@MacBook-Air-Stanislav.local>
2020-02-18 17:39:20 +0300

    diff generating refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import path from 'path';
 import fs from 'fs';
 import parse from './parsers';
-import genDiff from './diff';
+import genDiff from './genDiff';
 import renderDiff from './formatters';
 
 export default (firstConfigPath, secondConfigPath, outputFormat) => {
-  const difference = genDiff(
+  const diffTree = genDiff(
     parse(fs.readFileSync(firstConfigPath, 'utf-8'), path.extname(firstConfigPath)),
     parse(fs.readFileSync(secondConfigPath, 'utf-8'), path.extname(secondConfigPath)),
   );
-  const result = renderDiff(difference, outputFormat);
+  console.log(diffTree);
+  const result = renderDiff(diffTree, outputFormat);
   return result;
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parsers = {
+const parsing = {
   '.json': (config) => JSON.parse(config),
   '.yaml': (config) => yaml.safeLoad(config),
   '.ini': (config) => ini.parse(config),
 };
 
-export default (config, ext) => parsers[ext](config);
+export default (config, ext) => parsing[ext](config);
